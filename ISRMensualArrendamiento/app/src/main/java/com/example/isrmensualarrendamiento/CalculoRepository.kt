ab5a05fc39a6c01be07a2294b3e380e42c800810
Cristian3g <cristian98mex@gmail.com>
2020-06-09 00:28:09 -0500

    .
    .

--- ISRMensualArrendamiento/app/src/main/java/com/example/isrmensualarrendamiento/CalculoRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.example.isrmensualarrendamiento
-
-class CalculoRepository(val calculoDao: CalculosDao) {
-    suspend fun guardarCalculo(calculo: CalculosRealizados) = calculoDao.guardarCalculos(calculo)
-    fun obtenerDatos() = calculoDao.obtenerCalculos()
-}
